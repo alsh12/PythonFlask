7e68b5d1d9d2ada7d7f6e54952522eba2b7c6c6c
alsh12 <asheth369@gmail.com>
2022-05-13 11:54:52 +0530

    Commiting change

--- app.py
+++ app.py
@@ -1,49 +1,9 @@
-from flask import Flask , render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/") #main route
+def home():
+    return "Hello world!"
 
-@app.route("/<name>/<age>")
-def hello_world(name="Alok",age=0):
-    return render_template("Jinja_intro.html",name=name,age=age)
-
-
-@app.route("/another")
-def another():
-    return """
-    <html>
-    <body>
-    <h1>Greeting </h1>
-    </body>
-    </html>
-    """
-
-
-@app.route("/data")
-def render_data_structures():
-
-    movies = [
-        "Amar Akbar Anthony",
-        "Yaarana",
-        "Golmaal"
-    ];
-    return render_template("data-structures.html",movies=movies)
-
-
-@app.route("/company")
-def render_company():
-    company = "Apple"
-    return render_template("condition_basics.html",company=company)
-
-
-@app.route("/loop")
-def render_loop():
-    friendList =[
-        "Alok",
-        "Ashish",
-        "Ketan",
-        "Praveen",
-        "Viram",
-        "Lalit"
-    ]
-    return render_template("loop.html",friendList=friendList)
+app.run()
